ecab90411cb42aaa6a9a7d873685cbf08812722f
Swarnim24540 <swarnims897@gmail.com>
2026-06-26 14:52:53 +0530

    Add files via upload

---
+++ Day 13/inputarray.c
@@ -0,0 +1,20 @@
+# include<stdio.h>
+int main()
+{
+    int arr[100],n,i;
+    printf("enter the size of the array: ");
+    scanf("%d",&n);
+
+    printf("enter %d elements:\n",n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("Array elements are:\n");
+    for(i=0;i<n;i++)
+    {
+    printf("%d ",arr[i]);
+    }
+    return 0;
+}
---
+++ Day 13/largest_smallest_element.c
@@ -0,0 +1,32 @@
+# include<stdio.h>
+int main()
+{
+    int arr[100],n,i;
+    int largest, smallest;
+    
+    printf("enter the size of the array: ");
+    scanf("%d",&n);
+
+    printf("enter %d elements:\n",n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+      
+    largest = arr[0];
+    smallest = arr[0];
+
+    for(i=1;i<n;i++)
+    {
+        if(arr[i]>largest)
+        largest = arr[i];
+
+        if(arr[i]< smallest)
+        smallest = arr[i];
+    }
+
+    printf("Largest element = %d\n",largest);
+    printf("Smallest element =%d\n", smallest);
+     
+    return 0;
+}
